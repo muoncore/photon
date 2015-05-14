a1e4aa8d679fe67624abf10a29de8b2598636aab
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-05-14 14:16:27 +0100

    Add http-kit as option for HTTP server

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.4"]
                  [clj-http "1.1.2"]
                  [org.clojure/data.json "0.2.6"]
+                 [http-kit "2.1.18"]
                  [clj-time "0.9.0"]
+                 [ring "1.3.2"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler eventstore.handler/app}
+  :ring {:handler eventstore.handler/app} ;; jetty
+  :main eventstore.handler ;; http-kit
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- src/eventstore/handler.clj
+++ src/eventstore/handler.clj
@@ -1,6 +1,9 @@
 (ns eventstore.handler
+  (:use [org.httpkit.server :only [run-server]])
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.reload :as reload]
+            [compojure.handler :refer [site]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
@@ -10,3 +13,9 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+;; Workaround to have http-kit as the provider for Ring
+;; In order to use http-kit, run `lein run` instead of `lein ring server`
+(defn -main [& args]
+  (let [handler (reload/wrap-reload app)]
+    (time (run-server handler {:port 3000}))))
