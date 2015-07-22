a74378fc947e5028ebd052fff486b1bbb5165333
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-07-22 14:28:49 +0100

    Upgrade muon-clojure to 0.1.14

--- test/eventstore/current/query_test.clj
+++ test/eventstore/current/query_test.clj
@@ -6,8 +6,8 @@
             [eventstore.muon :as muon])
   (:use midje.sweet))
 
-#_(def amazon-url "amqp://muon:microservices@msg.cistechfutures.net")
-(def amazon-url "amqp://localhost")
+(def amazon-url "amqp://muon:microservices@msg.cistechfutures.net")
+#_(def amazon-url "amqp://localhost")
 
 #_(let [m (cl/muon-client amazon-url "client-test" "client" "test")
       #_#_db (filedb/->DBFile (clojure.java.io/resource "events.json"))
@@ -30,7 +30,8 @@
 (let [m (cl/muon-client amazon-url "client-test" "client" "test")]
   (Thread/sleep 5000)
   (let [res (cl/with-muon m
-              (cl/post-event "muon://eventstore/events" "chatter"
+              (cl/query-event "muon://eventstore/projection" {:projection-name "SimpleTest"})
+              #_(cl/post-event "muon://eventstore/events" "chatter"
                              {"service-id","muon://chatter","local-id","5198cea7-b260-4bb7-963d-f102132032fe","payload",{"id","dbd6eecf-8f5c-42aa-8aa8-1b2172d53c71","text","substitutable","textanalysis",{"aggregateSentiment",40,"keyphrases",[{"phrase","substitutable","count",1}]}},"stream-name","chatter","server-timestamp",1420660080000}))]
     (log/info res)
     res))
