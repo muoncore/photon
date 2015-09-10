211da5601d4f35ae1fabe05cb9660492b0388633
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-09-10 18:13:09 +0100

    Tests for projections

--- test/photon/current/integration_test.clj
+++ test/photon/current/integration_test.clj
@@ -5,7 +5,8 @@
             [photon.config :as conf]
             [photon.filedb :as filedb]
             [photon.db :as db]
-            [photon.muon :as muon])
+            [photon.muon :as muon]
+            [photon.current.common :refer :all])
   (:use midje.sweet))
 
 (defn elem-count [ch]
@@ -14,37 +15,11 @@
       n
       (recur (<!! ch) (inc n)))))
 
-(defn post-one-event
-  ([m url]
-   (cl/with-muon m
-     (cl/post-event (str "muon://" url "/events")
-                    {"service-id","muon://chatter",
-                     "local-id","5198cea7-b260-4bb7-963d-f102132032fe",
-                     "payload",{"id","dbd6eecf-8f5c-42aa-8aa8-1b2172d53c71",
-                                "text","substitutable",
-                                "textanalysis",
-                                {"aggregateSentiment",40,
-                                 "keyphrases",[{"phrase",
-                                                "substitutable",
-                                                "count",1}]}},
-                     "stream-name","chatter",
-                     "server-timestamp",1420660080000})
-     #_(cl/query-event "muon://photon/projection"
-                       {:projection-name "count"}))) 
-  ([m]
-   (post-one-event m "photon-integration-test")))
-
-(let [temp-file (.getAbsolutePath
-                 (java.io.File/createTempFile "muon" ".json"))
-      d (filedb/->DBFile temp-file)
-      _ (println temp-file)
-      _ (db/delete-all! d)
-      uuid (java.util.UUID/randomUUID)
-      ms (muon/start-server! (str "photon-integration-test-" uuid) d)
+(let [uuid (java.util.UUID/randomUUID)
+      ms (new-server uuid)
       m (cl/muon-client (:amqp.url conf/config) "client-test"
                         "client" "test")
       res (post-one-event m (str "photon-integration-test-" uuid))
-      _ (Thread/sleep 2000)
       ch (cl/with-muon m
            (cl/stream-subscription (str "muon://photon-integration-test-"
                                         uuid "/stream")
@@ -55,7 +30,7 @@
   (fact "One event on stream" (elem-count ch) => 1)
   (dorun (take 10 (repeatedly
                    (fn []
-                     (post-one-event
+                      (post-one-event
                       m (str "photon-integration-test-" uuid))))))
   (let [ch (cl/with-muon m
              (cl/stream-subscription (str "muon://photon-integration-test-"
@@ -87,15 +62,4 @@
                                      :from 0))]
     (Thread/sleep 20000)
     (fact "11011 events on stream" (elem-count ch) => 11011)))
-
-
-
-#_(let [m (cl/muon-client amazon-url "client-test" "client" "test")]
-  (Thread/sleep 5000)
-  (let [res (cl/with-muon m
-              #_(cl/query-event "muon://photon/projection" {:projection-name "test"})
-              (dorun (take 10 (repeatedly (fn [] (cl/post-event "muon://photon/events" "chatter"
-                                                        {"service-id","muon://chatter","local-id","6198cea7-b260-4bb7-963d-f102132032fe","payload",{"id","dbd6eecf-8f5c-42aa-8aa8-1b2172d53c71","text","substitutable","textanalysis",{"aggregateSentiment",40,"keyphrases",[{"phrase","substitutable","count",1}]}},"stream-name","dummy","server-timestamp",1420660080000}))))))]
-    (log/info res)
-    res))
 
