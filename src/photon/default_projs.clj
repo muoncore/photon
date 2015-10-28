79e1094dccd3faf93e4644cd9ac5ac66d9ee3e62
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-28 17:12:23 +0000

    Clean up unnecessary code

--- src/photon/default_projs.clj
+++ src/photon/default_projs.clj
@@ -14,10 +14,6 @@
      {(:stream-name n)
       {:total-events 1}})))
 
-(def cambio-updates
-  (sfn/fn [p n]
-    ))
-
 (def default-projections
   [{:projection-name "__streams__"
     :stream-name "__all__"
